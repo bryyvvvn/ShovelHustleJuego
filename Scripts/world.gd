667d4cd5f72f4b061bd7b99cf920ab79c003335c
Nabil Ignacio Haddad Pomar <nabil.haddad@alumnos.ucn.cl>
2025-05-07 00:50:15 -0400

    Actualizacion bloques adyacentes e inicio pala

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -5,24 +5,32 @@ extends Node
 
 var player : CharacterBody2D
 var tilemap : TileMap
+var pos_anterior = Vector2i(0,0)
 
 
 func _ready():
 	tilemap = get_node("TileMap")
+	
+func bloque_cavado(cell: Vector2i) -> void:
+	tilemap.set_cell(3,cell, 0, Vector2(13,0),0)
+	print(cell)
 
 func cuadros_alrededor(pos: Vector2i) -> void:
-	for child in get_children():
-		if child.name != "TileMap":  # Verifica si el nombre del nodo no es "TileMap"
-			remove_child(child)
-			child.queue_free()
-		
+	
+	for i in range(-1, 2):
+		for j in range(-1, 2):
+			var offset = Vector2i(i, j)
+			var cell = pos_anterior + offset
+			var tile_id = tilemap.get_cell_source_id(0, cell)
+			if i!=0 or j != 0:
+				tilemap.set_cell(0,cell,-1)
+	
 	for i in range(-1, 2):
 		for j in range(-1, 2):
 			var offset = Vector2i(i, j)
 			var cell = pos + offset
 			var tile_id = tilemap.get_cell_source_id(0, cell)
-			if tile_id != -1 and i != 0 or j != 0:
-				var tile = tile_scene.instantiate()
-				tile.position = tilemap.map_to_local(cell)
-				add_child(tile)
-#s
+			if i!=0 or j != 0:
+				tilemap.set_cell(0,cell, 0, Vector2(11,0),0)
+	pos_anterior = pos
+				
