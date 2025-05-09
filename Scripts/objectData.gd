499c3a529349589f12e4437d18e57f55e9d1fac0
pruno <36778730+prun0bruno@users.noreply.github.com>
2025-05-09 18:53:48 -0400

    df
    dcfdf

---
+++ Scripts/objectData.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name objectData
+@export var nombre : String
+@export var tipo : String
+@export var valor : int
+@export var rareza : float
+@export var descripcion : String
