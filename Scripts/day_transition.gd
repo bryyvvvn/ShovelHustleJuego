289f9bb32219f449a461fe57f1dc43b4b3bb87ef
pruno <36778730+prun0bruno@users.noreply.github.com>
2025-05-14 22:09:22 -0400

    commit 2

--- Scripts/day_transition.gd
+++ Scripts/day_transition.gd
@@ -5,16 +5,26 @@ signal transition_done(success: bool)
 @onready var fade := $fade
 @onready var dia_label := $dayInfo/diaLabel
 @onready var cuota_label := $dayInfo/cuotaLabel
-@onready var money_label := $dayInfo/moneyLabel
 @onready var resultado_label := $dayInfo/resultLabel
 @onready var continuar_btn := $dayInfo/continuarBtn
+@onready var money_label := $dayInfo/dineroAnimado/moneyLabel
 
 var jugador_tiene_dinero := true
+
+#para animación
+var dinero_actual := 0
+var dinero_final := 0
 
 func setup(dia: int, dinero: int, cuota: int, tiene_dinero: bool):
 	dia_label.text = "Día %d" % dia
 	cuota_label.text = "Cuota diaria: $%d" % cuota
-	money_label.text = "Dinero disponible: $%d" % dinero
+	
+	dinero_actual = dinero
+	dinero_final = max(dinero - cuota, 0)
+	
+	money_label.text = "$%d" % dinero_actual
+	animar_descuento()
+	
 	jugador_tiene_dinero = tiene_dinero
 
 	if not tiene_dinero:
@@ -24,7 +34,7 @@ func setup(dia: int, dinero: int, cuota: int, tiene_dinero: bool):
 		resultado_label.text = ""
 		continuar_btn.text = "Continuar"
 
-	# Fundido a negro
+	# fundido a negro
 	fade.color.a = 1.0
 	var tween = create_tween()
 	tween.tween_property(fade, "color:a", 0.0, 1.5)
@@ -35,3 +45,13 @@ func _on_continuarBtn_pressed():
 		queue_free()
 	else:
 		get_tree().quit()
+		
+func animar_descuento():
+	var tween = create_tween()
+	tween.set_parallel(false)  
+
+	var duracion := 1.5
+	tween.tween_method(
+		func(valor): money_label.text = "$%d" % valor,
+		dinero_actual, dinero_final, duracion
+	)
