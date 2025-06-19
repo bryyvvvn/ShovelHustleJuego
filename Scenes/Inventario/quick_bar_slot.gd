6ee0e9758ad20f3be276e332db93a81562d6996e
NoIdeaA <antonio.tabilo.programacion@gmail.com>
2025-06-19 19:33:30 -0400

    arreglado error en ejecucion de quickbar

--- Scenes/Inventario/quick_bar_slot.gd
+++ Scenes/Inventario/quick_bar_slot.gd
@@ -6,12 +6,10 @@ extends Button
 func update_to_slot(slot: InvSlot) -> void:
 	if !slot.item:
 		item_stack_gui.visible = false
-		background_sprite.frame = 0
 		return
 	
 	item_stack_gui.InventorySlot = slot
 	item_stack_gui.update()
 	item_stack_gui.visible = true
 	
-	background_sprite.frame = 1 
 	
