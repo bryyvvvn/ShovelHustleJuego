03b13bfe0ea754b1ea6d0130cb621d6e95f2c2aa
pruno <36778730+prun0bruno@users.noreply.github.com>
2025-07-03 14:48:50 -0400

    ns
    ns

--- Scripts/Menú/multiplayermenu.gd
+++ Scripts/Menú/multiplayermenu.gd
@@ -3,11 +3,18 @@ extends Control
 @onready var _client: WebSocketClient = WebSocketClient
 @onready var player_list: ItemList = $Panel/VBoxContainer/jugadores
 @onready var status_label: Label = $Panel/VBoxContainer/estado
+@onready var chat_display: TextEdit = $Panel/TextEdit
+@onready var input_message: LineEdit = $Panel/HBoxContainer/LineEdit
+@onready var send_button: Button = $Panel/HBoxContainer/enviar
+@onready var desconectarse: Button = $Button
 
 var _host = "ws://ucn-game-server.martux.cl:4010/"
 var _my_id = ""
 
-
+var unmsg = '{
+	"event": "match-request-received",
+	"msg": "te invitaron a jugar"
+}'
 func _ready():
 	GlobalSettings.change_displayMode(0)
 	_client.connected_to_server.connect(_on_connected)
@@ -19,8 +26,11 @@ func _ready():
 		status_label.text = "Conexión fallida."
 
 	player_list.item_activated.connect(_on_player_selected)
-
+	
+	
+	#_show_invite_popup(unmsg)
 func _on_connected():
+	
 	status_label.text = "Conectado. Cargando jugadores..."
 	_send_get_user_list()
 
@@ -29,10 +39,12 @@ func _on_disconnected():
 
 func _on_message_received(message: String):
 	var msg = JSON.parse_string(message)
+	print("Mensaje recibido:", message)
 	match msg.event:
 		"connected-to-server":
 			_my_id = msg.data.id  #agregar forma de ponerse nombre
 			status_label.text = "Conectado como %s" % _my_id
+			desconectarse.text = "DESCONECTARSE"
 		"get-connected-players":
 			_update_player_list(msg.data)
 		"player-connected":
@@ -40,16 +52,17 @@ func _on_message_received(message: String):
 		"player-disconnected":
 			_remove_player(msg.data.id)
 		"match-request-received":
-			_show_invite_popup(message)
+			_show_invite_popup(msg)
 		"match-accepted":
 			status_label.text = "Match con %s" % msg.data.opponent
 			get_tree().change_scene_to_file("res://scenes/multiplayer/multiplayer.tscn")
 		"send-match-request":
 			if msg.status == "OK":
-				status_label.text = "Invitación enviada a %s" % msg.msg
+				status_label.text = msg.msg
 			else:
 				status_label.text = "Ha ocurrido un error"
-				
+		"public-message":
+			_sendToChatDisplay("%s: %s" % [msg.data.id, msg.data.msg])
 
 func _send_get_user_list():
 	var msg = { "event": "get-connected-players" }
@@ -68,29 +81,100 @@ func _remove_player(id: String):
 			return
 
 func _on_player_selected(index: int):
-	var target = player_list.get_item_text(index)
+	var target = player_list.get_item_text(index).strip_edges() 
+	#print(target)
 	var msg = {
 		"event": "send-match-request",
 		"data": {
 			"playerId": target,
-  }
+ 		 }
 	}
+	status_label.text = "Enviando solicitud..."
 	_client.send(JSON.stringify(msg))
+
 
 func _show_invite_popup(message: String):
 	var mensaje = JSON.parse_string(message)
 	var popup = AcceptDialog.new()
+	popup.dialog_text = "¿Quieres aceptar la partida?"
+	popup.title = "Invitación"
+	popup.get_ok_button().text = "Aceptar"
+	var cancel_button = Button.new()
+	cancel_button.text = "NO"
 	popup.dialog_text = mensaje.msg
 	popup.confirmed.connect(func():
 		var msg = {
-			"event": "accept-match", #AGREGAR LA REAL INTERACCIÓN. SE ABRE JUEGO Y SE EMPIEZAN A MANDAR MENSAJES CONSTANTEMENTE EN ESTE MISMO FORMATO var msg = { "event" : "ataque.ejemplo" } y data del jugador blabla
+			"event": "accept-match"
 		}
 		_client.send(JSON.stringify(msg))
 	)
+	cancel_button.pressed.connect(func(): 
+		var msg = {
+			"event": "reject-match"
+			}
+		_client.send(JSON.stringify(msg)) 
+		)
+	popup.add_child(cancel_button)
 	add_child(popup)
 	popup.popup_centered()
 
 
 func _on_button_pressed() -> void:
-	WebSocketClient.close()
-	get_tree().change_scene_to_file("res://Scenes/Menu/menujugar.tscn")
+	print("se apretó salir")
+	#var msg = {
+  	#	"event": "login",
+  	#	"data": {
+	#		"gameKey": "BMJWU0"
+  	#	}
+	#}
+	#_client.send(JSON.stringify(msg))
+	#WebSocketClient.close()
+	#get_tree().change_scene_to_file("res://Scenes/Menu/menujugar.tscn")
+
+
+
+##CONTROLES PARA EL CHAT 
+
+func _on_input_submitted(message:String): 
+	if input_message.text == "":
+		return
+		
+	_sendMessage(message)
+	input_message.text = ""
+
+func _on_send_pressed():
+	if input_message.text == "":
+		return
+
+	_sendMessage(input_message.text)
+	input_message.text = ""
+
+
+# Agrega un mensaje en la pantalla de chat
+func _sendToChatDisplay(msg):
+	print(msg)
+	chat_display.text += str(msg) + "\n"
+
+# Envía un mensaje a un usuario o al chat grupal y manda la solicitud al servidor
+func _sendMessage(message: String, userId: String = ''):
+	var action
+	if (userId != ''):
+		action = 'send-private-message'
+	else:
+		action = 'send-public-message'
+		
+	var dataToSend = {
+		"event": action,
+		"data": {
+			"message": message
+		}
+	}
+
+	_client.send(JSON.stringify(dataToSend))
+
+# Solicita la lista de usuarios activos al servidor
+func _sendGetUserListEvent():
+	var dataToSend = {
+		"event": 'get-connected-players'
+	}
+	_client.send(JSON.stringify(dataToSend))
